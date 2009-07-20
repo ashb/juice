73fe41ccd25c55645242f230ff8ae4cd8c5df143
Ash Berlin <ash_github@firemirror.com>
2009-07-20 23:51:13 +0100

    Rip things out and start again: JSGI-ify things (mostly)

--- t/live/juice.sh
+++ t/live/juice.sh
@@ -2,4 +2,4 @@
 cd ../..
 export DYLD_LIBRARY_PATH=~/code/js/mozjs_debug/lib:~/code/js/flusspferd/build/default/src
 export DYLD_FRAMEWORK_PATH=~/Library/Frameworks
-exec /Users/ash/code/js/flusspferd_install/bin/flusspferd $@
+exec /usr/local/bin/flusspferd -I lib $@
--- t/live/juice_cgi.js
+++ t/live/juice_cgi.js
@@ -1,25 +1,15 @@
-i = new Importer();
-i.context = this;
-i.paths.unshift('lib');
-import = Function.bind(i, 'load');
-warn = Function.bind(IO.stderr, 'print');
 
-import('juice.engine.cgi');
-import('juice.application');
+var cgi = require('juice/engine/cgi');
 
-var app = new Juice.Application;
-
-app.do_index = function do_index(req, res) {
-  IO.stderr.print("in do_index");
-  res.content = req.headers;
-  res.contentType = 'text/plain';
+function application(env) {
+  return [
+    200,
+    {contentType: 'text/html'},
+    function() {
+      yield "Hi!";
+      yield "<h2>Title</h2>";
+    }
+  ];
 }
 
-app.urlpatterns = [
-  ['/', app.do_index]
-];
-
-var engine = new Juice.Engine.CGI();
-engine.register_application('http://localhost:9090/a', app)
-
-engine.run();
+cgi.run(application);
