d77304df34c4c164c7d490c73801013f92f69e08
Ash Berlin <ash_github@firemirror.com>
2009-09-12 17:42:39 +0100

    Use the new dispatcher framework

--- t/live/juice_cgi.js
+++ t/live/juice_cgi.js
@@ -1,19 +1,24 @@
 
-var cgi = require('juice/engine/cgi');
-
-function application(env) {
+function doneChore(env, task_id) {
   return [
     200,
     {contentType: 'text/html'},
     function() {
-      yield "Hi!";
-      yield "<h2>Title</h2>";
       yield "<b>Hi!</b>";
       yield "<pre>Script url args are " + env.PATH_INFO + "</pre>";
       yield "<pre>This script is rooted at " + env.SCRIPT_NAME + "</pre>";
       yield "<h2>Title \u2603</h2>";
     }
   ];
+
 }
 
-cgi.run(application);
+var juice = require('lib/juice');
+
+var app = new juice.Application;
+
+app.actions = {
+  "/done/(\\d+)": doneChore
+};
+
+juice.run(app);
