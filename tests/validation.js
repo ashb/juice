1847e8382bc5e8a9ad20fca42d8eaa0a13751c95
Dominic Baggott <dominic.baggott@gmail.com>
2009-09-29 23:54:20 +0100

    Added some tests for and slightly changed the behaviour of Validation.validateField

--- tests/validation.js
+++ tests/validation.js
@@ -1,10 +1,34 @@
 let test = require('test'),
     asserts = test.asserts,
-    v =  require('juice/validation').Validation.prototype.validators;
+    Validation = require('juice/validation').Validation;
+
+// simple wrapper for testing throws
+asserts.throws = function( testcase, expected, message ) {
+  if ( message == undefined ) {
+    message = expected;
+    expected = undefined;
+  }
+  try {
+    testcase();
+    asserts.ok( 0, message );
+    asserts.diag( "No error thrown" );
+  }
+  catch ( e ) {
+    if ( expected === undefined || expected == e.toString() ) {
+      asserts.ok( 1, message );
+    }
+    else {
+      asserts.ok( 0, message );
+      asserts.diag( "   Got: " + e.toString() );
+      asserts.diag( "Wanted: " + expected );
+    }
+  }
+}
 
 if (!this.exports) this.exports = {};
 
 exports.test_Trim = function () {
+  var v = Validation.prototype.validators;
   asserts.same( v.trim( undefined ), undefined, "|undefined| should stay the same" );
   asserts.same( v.trim( "" ), "", "Empty string should stay the same" );
   asserts.same( v.trim( " \t " ), "", "Pure whitespace should result in an empty string" );
@@ -17,6 +41,7 @@ exports.test_Trim = function () {
 
 // Should this handle anything more than strings and numbers?
 exports.test_NotEmpty = function() {
+  var v = Validation.prototype.validators;
   asserts.same( v.notEmpty( undefined ), undefined, "|undefined| should fail" );
   asserts.same( v.notEmpty( "" ), undefined, "Empty string should fail" );
   asserts.same( v.notEmpty( "foo" ), "foo", "Non-empty strings should return a string" );
@@ -25,6 +50,7 @@ exports.test_NotEmpty = function() {
 }
 
 exports.test_Integer = function () {
+  var v = Validation.prototype.validators;
   asserts.same( v.integer( undefined ), undefined, "|undefined| should fail" );
   asserts.same( v.integer( "" ), "", "Empty string should pass" );
   asserts.same( v.integer( "foo" ), undefined, "Non-numeric string should fail" );
@@ -37,11 +63,76 @@ exports.test_Integer = function () {
 
 // 0 is neither positive nor negative
 exports.test_Positive = function () {
+  var v = Validation.prototype.validators;
   asserts.same( v.positive( -123 ), undefined, "Negative integers should fail" );
   asserts.same( v.positive( -123.456 ), undefined, "Negative floats should fail" );
   asserts.same( v.positive( 0 ), undefined, "Zero should fail" );
   asserts.same( v.positive( 123 ), 123, "Positive integers should pass" );
   asserts.same( v.positive( 123.456 ), 123.456, "Positive floats should pass" );
+}
+
+exports.test_ValidateFieldChaining = function () {
+  var v = new Validation();
+  var chained = { validation : [ "notEmpty", "integer", "positive" ] };
+  asserts.throws( function() { v.validateField( chained, "" ); }, "Empty field should throw error" );
+  asserts.throws( function() { v.validateField( chained, "foo" ); }, "Non integer should throw error" );
+  asserts.throws( function() { v.validateField( chained, "-123" ); }, "Negative integer should throw an error" );
+  asserts.same( v.validateField( chained, "123" ), 123, "Positive integer should be parsed into a number" );
+}
+
+exports.test_ValidateFieldMessages = function () {
+  Validation.messages.test = {};
+  var v = new Validation( undefined, "test" ); // language is "test" for error messages
+
+  // |message| present
+  var one_message = {
+    validation : [ "notEmpty", "integer" ],
+    message : "one message"
+  };
+  asserts.throws( function() { v.validateField( one_message, "" ); },
+                  "one message",
+                  "All errors should throw single message" );
+  asserts.throws( function() { v.validateField( one_message, "foo" ); },
+                  "one message",
+                  "All errors should throw single message" );
+
+  // |messages| present
+  var many_messages = {
+    validation : [ "notEmpty", "integer" ],
+    messages : {
+      "notEmpty" : "message for notEmpty",
+      "integer" : "message for integer"
+    }
+  };
+  asserts.throws( function() { v.validateField( many_messages, "" ); },
+                  "message for notEmpty",
+                  "notEmpty should throw its given message" );
+  asserts.throws( function() { v.validateField( many_messages, "foo" ); },
+                  "message for integer",
+                  "integer should throw its given message" );
+
+  // neither |message| nor |messages| present
+  var no_messages = {
+    validation : [ "notEmpty", "integer" ]
+  };
+  asserts.throws( function() { v.validateField( no_messages, "" ); },
+                  "notEmpty",
+                  "notEmpty should throw its name" );
+  asserts.throws( function() { v.validateField( no_messages, "foo" ); },
+                  "integer",
+                  "integer should throw its name" );
+
+  // default messages
+  Validation.messages.test = {
+    "notEmpty" : "default for notEmpty",
+    "integer" : "default for integer"
+  };
+  asserts.throws( function() { v.validateField( no_messages, "" ); },
+                  "default for notEmpty",
+                  "notEmpty should throw it's default message" );
+  asserts.throws( function() { v.validateField( no_messages, "foo" ); },
+                  "default for integer",
+                  "integer should throw it's default message" );
 }
 
 if (require.main == module)
