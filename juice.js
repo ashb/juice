8ce1dbc71eb3de3fcf815180c4611ec8b139a049
Dominic Baggott <dominic.baggott@gmail.com>
2009-10-14 22:52:45 +0100

    Add OS specific quick-start instructions

---
+++ juice.js
@@ -0,0 +1,38 @@
+$( function() {
+  // detect OS
+  if ( navigator.platform.indexOf( 'Mac' ) != -1 ) $( '#mac-os-x' ).addClass( 'detected' );
+  if ( navigator.platform.indexOf( 'Win' ) != -1 ) $( '#windows' ).addClass( 'detected' );
+  if ( navigator.platform.indexOf( 'Linux' ) != -1 ) $( '#linux' ).addClass( 'detected' );
+
+  // hide everything but the detected OS, if one was found
+  $( '#getting-started .detected' ).each( function() {
+    var oses = [];
+
+    // build links to reveal each of the OS blocks
+    $( '#getting-started div' ).each( function() {
+      ( function( os ) {
+        var link = $( '<a>' )
+          .text( $( 'h3 em', os ).text() )
+          .attr( 'href', '#' + os.id )
+          .click( function( e ) {
+            e.preventDefault();
+            $( '#getting-started div' ).hide();
+            $( os ).show();
+          } );
+        oses.push( link );
+      } )( this );
+    } ).not( this ).hide();
+
+    // stick the alternative OS links somewhere visible
+    $( '<p>' )
+      .attr( 'id', 'os-alternatives' )
+      .text( 'Need instructions for ' )
+      .append( oses[ 0 ] )
+      .append( ', ' )
+      .append( oses[ 1 ] )
+      .append( ' or ' )
+      .append( oses[ 2 ] )
+      .append( '?' )
+      .appendTo( '#getting-started' );
+  } );
+} );
