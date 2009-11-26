a4f02ad852cd022f30b4997ebbe8f77a50744194
Dominic Baggott <dominic.baggott@gmail.com>
2009-11-26 17:58:06 +0000

    Track downloads via Google Analytics.

--- juice.js
+++ juice.js
@@ -1,38 +1,9 @@
+// track downloads
 $( function() {
-  // detect OS
-  if ( navigator.platform.indexOf( 'Mac' ) != -1 ) $( '#mac-os-x' ).addClass( 'detected' );
-  if ( navigator.platform.indexOf( 'Win' ) != -1 ) $( '#windows' ).addClass( 'detected' );
-  if ( navigator.platform.indexOf( 'Linux' ) != -1 ) $( '#linux' ).addClass( 'detected' );
-
-  // hide everything but the detected OS, if one was found
-  $( '#installation .detected' ).each( function() {
-    var oses = [];
-
-    // build links to reveal each of the OS blocks
-    $( '#installation div' ).each( function() {
-      ( function( os ) {
-        var link = $( '<a>' )
-          .text( $( 'h3 em', os ).text() )
-          .attr( 'href', '#' + os.id )
-          .click( function( e ) {
-            e.preventDefault();
-            $( '#installation div' ).hide();
-            $( os ).show();
-          } );
-        oses.push( link );
-      } )( this );
-    } ).not( this ).hide();
-
-    // stick the alternative OS links somewhere visible
-    $( '<p>' )
-      .attr( 'id', 'os-alternatives' )
-      .text( 'Need instructions for ' )
-      .append( oses[ 0 ] )
-      .append( ', ' )
-      .append( oses[ 1 ] )
-      .append( ' or ' )
-      .append( oses[ 2 ] )
-      .append( '?' )
-      .appendTo( '#installation' );
+  $( "a[href^='/downloads']" ).bind( "click", function() {
+    // check pageTracker is available first
+    if ( pageTracker ) {
+      pageTracker._trackPageview( this.href );
+    }
   } );
 } );
