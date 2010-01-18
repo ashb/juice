8b41d9f64dc0cd87fa83e00ed01b8fc6b5f9cd8a
Ash Berlin <ash_github@firemirror.com>
2010-01-18 17:23:41 +0000

    Helper function to make mocking modules in tests nicer

--- test/lib/qmock.js
+++ test/lib/qmock.js
@@ -27,6 +27,27 @@ function no_submodule() {
   throw "qmock submodule not found!";
 }
 
+exports.mockForTest = function( mocks, test ) {
+  return function() {
+    var mocks_to_remove = {};
+
+    try {
+      record_mocks = mocks_to_remove;
+      exports.unloadJuice();
+      mocks();
+      return test();
+    }
+    finally {
+      for ( var i in mocks_to_remove ) {
+        // QMock has some bugs in mocked members with varying interfaces it seems
+        // exports.verifyOk( mocks_to_remove[ i ], i + " mocked ok" );
+        delete require.module_cache[ i ];
+      }
+    }
+  }
+}
+
+var record_mocks = void 0;
 // Flusspferd specific - mock up a module
 exports.mockModule = function(module, api) {
   var mock;
@@ -37,14 +58,26 @@ exports.mockModule = function(module, api) {
     mock = new exports.Mock( api );
 
   var cache;
-  // TODO: Maybe we should change the exports in place if its already loaded?
   if ( "exports" in require.module_cache.flusspferd )
        cache = { exports: mock };
   else cache = mock;
 
   require.module_cache[ module ] = cache;
 
+  if (record_mocks)
+
+    record_mocks[module] = mock;
+
   return mock;
+}
+
+// Flusspferd specific - unload all juice modules.
+exports.unloadJuice = function() {
+  for ( var i in require.module_cache ) {
+    if ( i.match( /\bjuice\b/ ) && !i.match( /\btest\b/ ) ) {
+      delete require.module_cache[i];
+    }
+  }
 }
 
 exports.verifyOk = function( mock, msg ) {
